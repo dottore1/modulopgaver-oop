114140a4a5e849352ed61c500e1c8ae8d269b670
nichlasboraso <54813290+dottore1@users.noreply.github.com>
2019-09-12 16:48:41 +0200

    upload :)

---
+++ src/modul/opgave/pkg4/ModulOpgave4.java
@@ -0,0 +1,61 @@
+package modul.opgave.pkg4;
+
+public class ModulOpgave4 {
+
+    public static void main(String[] args) {
+
+        int[][] array = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for(int x = 0 ; x < 9; x++){
+                array[i][x] = x + 1;
+            }
+        }
+
+        for (int j = 0; j < 9; j++) {
+
+            for (int k = 0; k < 9; k++) {
+                System.out.println(array[j][k]);
+            }
+        }
+
+    }
+}
+/*
+        int array_length = 12;
+        int[] array = new int[12];
+
+        for (int i = 0; i < array.length; i += 1) {
+            array[i] = (int) (Math.random() * 100);
+
+        }
+        for (int a : array) {
+            if (a % 3 == 0) {
+                System.out.println(a + " The number is in 3 tabellen");
+
+                
+            } else {
+                System.out.println(a + " The number is not in 3 tabellen");
+            }
+        }
+ */
+ /*
+            for (int i : array){
+                if (i >= value) {
+                    System.out.println(i);
+                    
+         
+    }
+}
+ */
+
+ /*
+        // opgave 1 
+        int[] hiArray = {12, 44, 55, 12, 55, 66, 99, 1, 2, 4};
+        int maxInt = hiArray[0];
+        for (int i : hiArray) {
+            if (i > maxInt) {
+                maxInt = i;
+            }
+        }
+        System.out.println(maxInt);
+ */
