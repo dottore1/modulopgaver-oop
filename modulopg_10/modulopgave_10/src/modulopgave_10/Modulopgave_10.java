d4c9a6d9d5a486ab89f160164e20b008f1b927a4
dottore1 <54813290+dottore1@users.noreply.github.com>
2019-10-06 12:32:41 +0200

    Opgave 10 upload(OOP)

---
+++ modulopg_10/modulopgave_10/src/modulopgave_10/Modulopgave_10.java
@@ -0,0 +1,63 @@
+package modulopgave_10;
+
+import java.util.*;
+
+public class Modulopgave_10 {
+
+    public static void main(String[] args) {
+        Date expire = new Date(10000000);
+        FoodItem fi = new FoodItem("Banan", 100, expire);
+
+        String[] materials = {"Twix", "Nars", "Bounty", "KitKat"};
+
+        NonFoodItem nfi = new NonFoodItem("Chokolade", 199, materials);
+
+        System.out.println(nfi.toString());
+        System.out.println("---------------");
+
+        //opgave 2
+        FoodItem[] foods = new FoodItem[10];
+
+        for (int i = 0; i < foods.length; i++) {
+            String name = "Food" + i;
+            FoodItem food = new FoodItem(name, 199, new Date(119, 9, i + 9));
+
+            if (foods[i] == null) {
+                foods[i] = food;
+
+            } else {
+                break;
+
+            }
+
+        }
+        for (int i = 0; i < foods.length; i++) {
+            if (foods[i] != null) {
+                System.out.println(foods[i].toString());
+            }
+        }
+
+        System.out.println("--------------");
+        //opgave 3 
+        NonFoodItem[] nonfoods = new NonFoodItem[10];
+        for (int i = 0; i < nonfoods.length; i++) {
+            String name = "NonFood " + i;
+            double price = i + 5 * i + 5 * i;
+            String[] items = {"Small", "Medium", "Large"};
+            NonFoodItem nonfood = new NonFoodItem(name, price, items);
+
+            if (nonfoods[i] == null) {
+                nonfoods[i] = nonfood;
+            } else {
+                break;
+            }
+
+        }
+        for (int i = 0; i < nonfoods.length; i++) {
+            if (nonfoods[i] != null) {
+                System.out.println(nonfoods[i].toString()); 
+            }
+        }
+    }
+
+}
